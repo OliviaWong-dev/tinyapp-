00884a6c9f52a2628a83b6a0423538f37ef970af
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-26 16:44:35 +0000

    added web form and send data to backend

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +10,10 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {
+  Math.random().toString(36).substring(2, 8);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,10 +31,18 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  console.log("hi");
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  Ï;
   const longURL = urlDatabase[shortURL];
   const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
