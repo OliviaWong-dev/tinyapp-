ef57e5429a0703a406d673dc43f7d5b98ce3a7a6
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-28 15:25:25 +0000

    added EDIT function

--- express_server.js
+++ express_server.js
@@ -15,28 +15,34 @@ function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 }
 
+// root path send "Hello"
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// urlDatabase in JSON
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// hello path send "Hello World"
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// urls path to render urls_index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// render NEW URL form
 app.get("/urls/new", (req, res) => {
   console.log("hi");
   res.render("urls_new");
 });
 
+// POST NEW form on urls path
 app.post("/urls", (req, res) => {
   const shortURL = req.params.shortURL;
   const userSubmittedLongURL = req.body.longURL; // Log the POST request body to the console
@@ -45,6 +51,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${generatedShortURL}`); // Respond with 'Ok' (we will replace this)
 });
 
+// shortURL site
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
@@ -52,15 +59,26 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// get request to DELTE
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(`${longURL}`);
 });
 
+// added DELETE function
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+// added EDIT function
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const userSubmittedLongURL = req.body.longURL;
+  urlDatabase[shortURL] = userSubmittedLongURL;
+  console.log(urlDatabase[shortURL]);
   res.redirect("/urls");
 });
 
