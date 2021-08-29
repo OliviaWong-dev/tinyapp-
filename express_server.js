4933f4ac64c9384a4b355ff6becd7c13e6449f94
Olivia Wong <olivia.wy.wong@gmail.com>
2021-08-29 21:41:33 +0000

    added register and draft register form

--- express_server.js
+++ express_server.js
@@ -101,6 +101,10 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
